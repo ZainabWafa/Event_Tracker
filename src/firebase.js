e35ffc2f55c784a16c62e7a3b4f91dbea0dfa7c1
zainab wafa <zainabwafarasheed8430@gmail.com>
2022-01-17 09:59:46 +0530

    First commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCRsWvRDSSXJKiDjHVS1wt7oZmKFm93ISY",
+  authDomain: "eventtracker-1bbe2.firebaseapp.com",
+  projectId: "eventtracker-1bbe2",
+  storageBucket: "eventtracker-1bbe2.appspot.com",
+  messagingSenderId: "420985177549",
+  appId: "1:420985177549:web:ea0dbd53d11678babcb084",
+  measurementId: "G-82FB9HK55T"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
